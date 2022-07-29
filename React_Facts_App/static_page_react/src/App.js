c6a962fcb08cbd80684d1f61f8d2edf188ecf389
Odunosh Moses <Odunoshomoses@gmail.com>
2022-07-29 11:25:54 +0000

    setup react properly and building first project markup

---
+++ React_Facts_App/static_page_react/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src="https://www.iconfinder.com/icons/1174949/js_react_js_logo_react_react_native_icon" alt=""/>
+        <h1>ReactFacts</h1>
+        <nav>React Course - Project 1</nav>
+      </header>
+      <main>
+        <h2>
+          Fun facts about React
+        </h2>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
