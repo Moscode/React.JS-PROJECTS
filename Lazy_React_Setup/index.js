71ed03334011fb44529fd9650acb625bbed59f1c
Odunosh Moses <Odunoshomoses@gmail.com>
2022-07-29 08:03:35 +0000

    [feat]: using composability system design principle of react

--- Lazy_React_Setup/index.js
+++ Lazy_React_Setup/index.js
@@ -1,6 +1,23 @@
 //ReactDOM.render(<p>It's going well</p>, document.getElementById("root"))
+function ListItems(){
+    return(
+        <ul>
+            <li>First list element</li>
+            <li>Second list element</li>
+        </ul>
+    )
+}
+
+function ParaGraph(){
+    return(
+        <p>I'm enjoying documenting my learning using my learning</p>
+    )
+}
 
 ReactDOM.render(
-    <ul><li>First list element</li><li>Second list element</li></ul>,
+    <div>
+        <ListItems />
+        <ParaGraph />
+    </div>,
     document.querySelector("#root")
     )
