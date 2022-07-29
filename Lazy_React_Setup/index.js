2af6d3670247c7277bf858a338da483538ed39ef
Odunosh Moses <Odunoshomoses@gmail.com>
2022-07-29 07:27:08 +0000

    [feat]: lazy react setup using react, react-dom, babel cdn

---
+++ Lazy_React_Setup/index.js
@@ -0,0 +1,6 @@
+//ReactDOM.render(<p>It's going well</p>, document.getElementById("root"))
+
+ReactDOM.render(
+    <ul><li>First list element</li><li>Second list element</li></ul>,
+    document.querySelector("#root")
+    )
