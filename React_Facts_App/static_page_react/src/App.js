91fb1e8e14c12989167067c8b342f733d6781557
Odunosh Moses <Odunoshomoses@gmail.com>
2022-08-08 22:08:38 +0000

    [feat]: seperate everything into components

--- React_Facts_App/static_page_react/src/App.js
+++ React_Facts_App/static_page_react/src/App.js
@@ -1,25 +1,7 @@
 import './App.css';
 import Header from "./header/Header";
-function Main(){
-  return(
-    <div className="main">
-      <h2> Fun facts about React </h2>
-      <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100k stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </div>
-  )
-}
-
-function Footer(){
-  return(
-    <small className="footer">@copyright Moscode</small>
-  )
-}
+import Footer from './footer/Footer';
+import Main from './main/Main';
 
 function App() {
   return (
---
+++ React_Facts_App/static_page_react/src/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import '../App.css'
+
+export default function Footer(){
+    return(
+      <small className="footer">@copyright Moscode</small>
+    )
+  }
